2057617ced17c763e0450b268295e126c58173db
watchmen-in <104987484+watchmen-in@users.noreply.github.com>
2025-08-09 20:31:12 -0400

    Update config.ts

--- src/map/config.ts
+++ src/map/config.ts
@@ -1,3 +1,3 @@
 export const STYLE_URL =
   import.meta.env.VITE_MAP_STYLE_URL ||
-  'https://basemaps.cartocdn.com/gl/positron-gl-style/style.json';
+  "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json";
